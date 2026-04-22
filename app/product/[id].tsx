ef3471791dc012b12ca8ea54afc17eb4756a8451
Imraj <imrajrabbani2003@gmail.com>
2026-04-22 20:38:12 +0600

    implement all products page

--- app/product/[id].tsx
+++ app/product/[id].tsx
@@ -26,7 +26,8 @@ const ProductDetail = () => {
     const [activeImageIndex, setActiveImageIndex] = useState(0);
 
     const fetchProduct = async () => {
-        setProduct(dummyProducts.find((product) => product._id === id) as any);
+        const found: any = dummyProducts.find((product) => product._id === id);
+        setProduct(found ?? null);
         setLoading(false);
     }
 
